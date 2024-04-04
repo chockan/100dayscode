46a66f156635165dfec5cba705f9427593a4dd79
chockan <chockansamy@gmail.com>
2024-04-04 13:32:07 +0530

    first

---
+++ 06-Day6-Variables-and-Data-Types/.tutorial/1.py
@@ -0,0 +1,17 @@
+
+
+
+# x=9.0
+# d222=float(x)
+# e=complex(x)
+# print(d222)
+# print(e)
+# print(type(d222))
+# print(type(e))
+
+x=input("enter the first number")
+y=input("enter the second number")
+a=int(x)
+b=int(y)
+
+print(a+b)
